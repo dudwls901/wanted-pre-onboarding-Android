73bf2ea38b8dcf9cafcd3d64b97901de39f784d0
yeongjin <dudwls901@naver.com>
2022-09-08 11:35:23 +0900

    [Chore] 패키징, 라이브러리 설정, app_name 변경

--- app/src/main/java/com/yeongjin/news/view/MainActivity.kt
+++ app/src/main/java/com/yeongjin/news/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yeongjin.news
+package com.yeongjin.news.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yeongjin.news.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
